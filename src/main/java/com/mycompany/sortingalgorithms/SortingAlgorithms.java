a6fb050c83743e03f2caf390ab36cbbd141b4cd6
anacleto-caua <anacleto.for.work@gmail.com>
2023-05-14 16:40:37 -0300

    Subindo arquivos base

---
+++ src/main/java/com/mycompany/sortingalgorithms/SortingAlgorithms.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sortingalgorithms;
+
+/**
+ *
+ * @author cauaa
+ */
+public class SortingAlgorithms {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

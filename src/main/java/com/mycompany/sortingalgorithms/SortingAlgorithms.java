07023002bde6d0819fc7dd85dc1bdcaea632444f
Rafael T <rafael.trigo.azevedo@gmail.com>
2023-05-14 18:58:13 -0300

    Preparação da main

--- src/main/java/com/mycompany/sortingalgorithms/SortingAlgorithms.java
+++ src/main/java/com/mycompany/sortingalgorithms/SortingAlgorithms.java
@@ -4,13 +4,52 @@
 
 package com.mycompany.sortingalgorithms;
 
+import java.util.Random;
+
 /**
  *
  * @author cauaa
  */
 public class SortingAlgorithms {
-
+    
+    //gera um vetor de 300.000 posicoes
+    public static void geraVetor(int[] vetor)
+    {
+        Random r = new Random();
+        
+        
+        for(int i = 0; i < vetor.length; i++)
+        {
+            //gera valores aleatorios entre 1 e o tamanho do vetor, que no caso é 300000
+            vetor[i] = r.nextInt(vetor.length) + 1;
+        }
+    }
+    
+    //imprime o vetor
+    public static void imprimeVetor(int vetor[], int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print(vetor[i] + ", ");
+        }
+    }
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        int[] vetor = new int[30];
+        
+//       for(int i = 0; i < 11; i++)
+//       {
+//           
+//       }
+
+        geraVetor(vetor);
+
+        imprimeVetor(vetor, 30);
+        System.out.println();
+        imprimeVetor(vetor, 10);
+        System.out.println();
+        imprimeVetor(vetor, 20);
+
     }
 }
